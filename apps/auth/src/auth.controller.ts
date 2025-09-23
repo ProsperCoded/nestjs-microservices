4d9ce82ad2a0066726bf6443a21bd5c83f94f685
Prosper Coded <enweremproper@gmail.com>
2025-09-23 17:02:11 +0100

    Implement user authentication with JWT, including login functionality and user verification. Enhance UsersService to hash passwords and verify credentials. Update AuthController to handle login requests and set authentication cookies.

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Post, Request, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalGuard } from './guards/local.guard';
+import { CurrentUser } from './decorator/current-user.decorator';
+import { UserDocument } from './users/schemas/user.schema';
+import type { Response } from 'express';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  @UseGuards(LocalGuard)
+  @Post('login')
+  async login(
+    @CurrentUser() user: UserDocument,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const jwt = await this.authService.login(user, response);
+    response.send(jwt);
   }
 }
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from 'apps/auth/src/users/dto/create-user.dto';
+import * as bcrypt from 'bcryptjs';
+import { UsersRepository } from 'apps/auth/src/users/users.repository';
 
 @Injectable()
 export class UsersService {
-  createUser(createUserDto: CreateUserDto) {
-    return 'User created';
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    return this.usersRepository.create({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    });
+  }
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Credentials are not valid.');
+    }
+    return user;
   }
 }
